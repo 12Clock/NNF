a0a4c03f7d493b4fefb03b2f451ab51ec1159af5
12Clock <fengjie1995@foxmail.com>
2020-06-23 21:41:55 +0800

    Delete extra files

--- NNF_Linux/src/FTen/NNFTensor.h
+++
@@ -1,7 +0,0 @@
-#ifndef NNF_Tensor_All
-#define NNF_Tensor_All
-
-#include "generic/NNFArray.h"
-#include "generic/NNFArray.cpp"
-
-#endif
--- NNF_Linux/src/FTen/cpu/NNFCAllocator.h
+++
@@ -1,6 +0,0 @@
-#ifndef NNFC_ALLOCATOR_H
-#define NNFC_ALLOCATOR_H
-
-#include<stdlib.h>
-
-#endif
